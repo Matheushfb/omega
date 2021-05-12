c3804ef819eefe27e0a165b1205461b306eaae95
root <matheushfbrandao@gmail.com>
2021-05-11 22:12:55 -0300

    update dev code

--- teste.py
+++ teste.py
@@ -1,22 +1,21 @@
 import Omega
 import sys
 import base64
+import dectobin
 
-text = open(sys.argv[1],'r')
-arquivo = text.encode('ascii') 
-base64_bytes = base64.b64encode(arquivo)
-o = Omega.Omega(arquivo, sys.argv[2])
+text = open(sys.argv[1],'rb')
+arquivo = text.read() 
+#base64_bytes = base64.b64encode(arquivo)
+o = Omega.Omega(text, sys.argv[2])
 
-for linha in arquivo:
-    basemontador = o.base_montador(len(linha))
-    montador = o.gera_montador(int(sys.argv[2]),basemontador)
-    string = o.permuta(montador,list(linha))
-    decimal = o.gera_decimal(basemontador,montador)
-    #print (basemontador)
-   # print (montador)
-    print (''.join(string))
-    #print (decimal)
-    print(linha)
+#for linha in arquivo:
+basemontador = o.base_montador(len(arquivo))
+montador = o.gera_montador(int(sys.argv[2]),basemontador)
+string = o.permuta(montador,list(arquivo))
+decimal = o.gera_decimal(basemontador,montador)
+#print(text)
+print(string)
+
 
 #o = Omega.Omega(arquivo, sys.argv[2])
 #basemontador = o.base_montador(len(arquivo))
