9235ab2ac5af09912c8e7a3a3a924727447f8dc3
matheus <matheushfbrandao@gmail.com>
2022-07-02 17:34:52 -0300

    string to list

--- binario.py
+++ binario.py
@@ -1,7 +1,12 @@
+import sys
 def converte_para_binario(string):
     d,b=[],[]
-    for i in nome:
+    for i in string:
         d.append(ord(i))
     for i in d:
         b.append(format(i,'08b'))
     return (b)
+
+string = sys.argv[1]
+
+print(list(converte_para_binario(string)[0]))
