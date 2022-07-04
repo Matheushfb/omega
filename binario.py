dda2a568e39e31c83a995094069c514013a03e86
matheus <matheushfbrandao@gmail.com>
2022-07-03 22:18:11 -0300

    criando string binaria.

--- binario.py
+++ binario.py
@@ -1,12 +1,18 @@
 import sys
-def converte_para_binario(string):
-    d,b=[],[]
-    for i in string:
-        d.append(ord(i))
-    for i in d:
-        b.append(format(i,'08b'))
-    return (b)
 
+class BinTools:
+    def __init__(self, string):
+        self.string = string
+
+    def converte_para_binario(self):
+        d,self.b=[],[]
+        for i in self.string:
+            d.append(ord(i))
+        for i in d:
+            self.b.append(format(i,'08b'))
+        return self.b
+    
 string = sys.argv[1]
-
-print(list(converte_para_binario(string)[0]))
+ba = BinTools(string)
+binn = "".join(ba.converte_para_binario())
+print(binn)
