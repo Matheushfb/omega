cbcd76848e7e7250922dbe0a279616e2f3ef8512
matheus <matheushfbrandao@gmail.com>
2022-07-04 17:33:53 -0300

    permutacao binaria

--- binario.py
+++ binario.py
@@ -12,7 +12,17 @@ class BinTools:
             self.b.append(format(i,'08b'))
         return self.b
     
+    def esteira_binaria(self,b):
+        i,j,k = 0,4,0
+        l = len(b)
+        while k < l:
+            b[i:j],b[j:(j+j)]=b[j:(j+j)],b[i:j]
+            i=i+4
+            j=j+4
+            k=k+1
+        return b
+
 string = sys.argv[1]
 ba = BinTools(string)
 binn = "".join(ba.converte_para_binario())
-print(binn)
+print(ba.esteira_binaria(list(binn)))
