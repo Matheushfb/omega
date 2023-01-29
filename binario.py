eebb00828f6a608ac55d9f339765e1be68eabd05
matheus.brandao <matheus.brandao@plataforma13.com.br>
2023-01-29 15:04:28 -0300

    binario.py

--- binario.py
+++ binario.py
@@ -13,15 +13,11 @@ class BinTools:
         return self.b
     
     def esteira_binaria(self,b):
-        i,j,k = 0,8,0
-        l = len(string)
-        print(b)
-        while k < l:
-            b[i:(i+4)],b[j:(j-4)]=b[j:(j-4)],b[i:(i+4)]
-            i=i+8
-            j=j+8
-            k=k+1
+        print(len(string))
+        print(len(b)//2)
+        print(b[:4])
         return b
+
 
 string = sys.argv[1]
 ba = BinTools(string)
